af1a9bd41a2dd9a3a797bf7bf81b68b1753014ca
André Pettersson <andre20030417@gmail.com>
2025-10-13 17:02:19 +0200

    Flytta lite från questmanager till quest

--- Quest.cs
+++ Quest.cs
@@ -3,5 +3,21 @@ namespace Portfolio_Projekt_Quest_Tracker
 {
     public class Quest
     {
+        // Attribut
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; }
+        public bool IsCompleted { get; set; }
+
+        // Konstruktor
+        public Quest(string title, string description, DateTime dueDate, string priority)
+        {
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            Priority = priority;
+            IsCompleted = false;
+        }
     }
 }
